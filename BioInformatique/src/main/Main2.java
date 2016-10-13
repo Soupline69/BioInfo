098b5725bea4ac950ac7e4c7e29cfdadf6dbe1ae
mikis.millemaci <mikis.millemaci@umons.ac.be>
2016-10-13 11:07:19 +0200

    Alignement semi-global (1)

--- BioInformatique/src/main/Main2.java
+++ BioInformatique/src/main/Main2.java
@@ -1,12 +1,15 @@
 package main;
 
-public class Main {
+import model.Matrice;
+import model.Position;
+
+public class Main2 {
 
 	public static void main(String[] args) {
 		// 1. Faire l'alignement semi-global avec deux fragments tels que :
 		
-		String f1 = "GTTCAAATCGGGCTT"; //AGCTTC // F1 = ATTAGACCATGCGGC // CAGCACTTGGATTCTCGG
-		String f2 = "CCAAGTCAAGTCGG"; // AGTCAGTGCGTGC // F2 = ATCGGCATTCAGT // CAGCGTGG
+		String f1 = "CCAAGTCAAGTCGG"; //AGCTTC // ATTAGACCATGCGGC // CAGCACTTGGATTCTCGG
+		String f2 = "GTTCAAATCGGGCTT"; // AGTCAGTGCGTGC // ATCGGCATTCAGT // CAGCGTGG
 		
 		System.out.println("f1 : "+f1.length()+" f2 : "+f2.length());
 		if(f1.length() < f2.length()) { // Permet de toujours avoir le fragment le plus long sur les lignes
@@ -16,57 +19,16 @@ public class Main {
 		}
 		System.out.println("f1 : "+f1.length()+" f2 : "+f2.length());
 		
-		int[][] m = new int[f1.length() + 1][f2.length() + 1];
 		
-		// Pour obtenir la matrice de l'exemple du pdf, ce serait la première étape // OK // 15/6 ou 6/15
+		Matrice m = new Matrice(f1, f2);
+		Position maxPosition = m.maxMatrice();
 		
-		// Calculer le résultat de la matrice
-		for(int i = 1; i < f1.length() + 1; i++) {
-			for(int j = 1; j < f2.length() + 1; j++) {
-				m[i][j] = max(
-							m[i-1][j] - 2, // Faire une constante avec le score d'un GAP
-							m[i][j-1] - 2,
-							m[i-1][j-1] + match(f1.charAt(i-1), f2.charAt(j-1))
-						);
-			}
-		}
-		
-		// Afficher le résultat de la matrice 
-	/*	for(int i = 0; i < f1.length() + 1; i++) {
-			for(int j = 0; j < f2.length() + 1; j++) {		
-				System.out.print(m[i][j]+"  ");
-			}
-			System.out.println("");
-		}*/
-		
-		// Trouver le maximum de la dernière ligne/dernière colonne et l'afficher
-		// Colonne max
-		int maxColonne = 0;
-		int ligne = -1;
-		for(int i = 0; i < f1.length() + 1; i++) {	
-			int comp = m[i][f2.length()];
-			if(comp > maxColonne) {
-				maxColonne = comp;
-				ligne = i;
-			}
-		}
-		// Ligne max
-		int maxLigne = 0;
-		int colonne = -1;
-		for(int i = 0; i < f2.length() + 1; i++) {	
-			int comp = m[f1.length()][i];
-			if(comp > maxLigne) {
-				maxLigne = comp;
-				colonne = i;
-			}
-		}
-		
-		if(maxLigne > maxColonne) {
-			System.out.println("\n\nLe maximum est en ["+(f1.length())+"]["+colonne+"] avec "+maxLigne+" (ligne)");
-			alignementLigne(f1, f2, f1.length(), colonne, m);
+		if(maxPosition.getX() == m.getCountLigne() - 1) {
+			System.out.println("\n\nLe maximum est en ["+(m.getCountLigne() - 1)+"]["+maxPosition.getY()+"] (ligne)");
+			alignementLigne(f1, f2, f1.length(), maxPosition.getY(), m.getMatrice());
 		} else {
-			System.out.println("\n\nLe maximum est en ["+ligne+"]["+(f2.length())+"] avec "+maxColonne+" (colonne)");
-			alignementColonne(f1, f2, ligne, f2.length(), m);
+			System.out.println("\n\nLe maximum est en ["+maxPosition.getX()+"]["+(m.getCountColonne() - 1)+"] (colonne)");
+			alignementColonne(f1, f2, maxPosition.getX(), f2.length(), m.getMatrice());
 		}
 	}
 
@@ -87,7 +49,10 @@ public class Main {
 			} else if(m[li][col] == m[li][col-1] - 2) { // Gap
 				temp = "-" + temp;
 			} else if(m[li][col] == m[li-1][col] - 2) { // Gap
-				temp = "-" + temp;
+				temp = f1.charAt(li) + temp;
+				String localBegin = f2.substring(0, col);
+				String localEnd = f2.substring(col);
+				f2 = localBegin + "-" + localEnd;
 				li--;
 			}
 			
@@ -102,23 +67,16 @@ public class Main {
 		}
 		
 		if(li == 0 && col != 0) {
-			for(int i = 0; i < col; i++) {
+			for(int i = 0; i < col+1; i++) {
 				temp = "-" + temp;
 			}
 		}
-		
-		
-		
-		System.out.println("colonne : "+col + " ligne : "+li);
-
 		
 		f1 = temp; 
 		
 		System.out.println("\n\n");
 		System.out.println(f1 + "    " + f1.length());
 		System.out.println(f2 + "    " + f2.length());
-		
-		
 	}
 	
 	public static void alignementLigne(String f1, String f2, int ligne, int colonne, int[][] m) {
@@ -136,8 +94,11 @@ public class Main {
 				col--;
 			} else if(m[li][col] == m[li-1][col] - 2) { // Gap
 				temp = "-" + temp;
-			} else if(m[li][col] == m[li][col-1] - 2) { // Gap
-				temp = "-" + temp;
+			} else if(m[li][col] == m[li][col-1] - 2) { // Gap // VERIFIER ICI QUAND TU VAS SUR LE COTE
+				temp = f2.charAt(col) + temp;
+				String localBegin = f1.substring(0, li);
+				String localEnd = f1.substring(li);
+				f1 = localBegin + "-" + localEnd;
 				col--;
 			}
 			
@@ -157,16 +118,6 @@ public class Main {
 		System.out.println(f2 + "    " + f2.length());
 	}
 	
-	public static int max(int a, int b, int c) {
-		if(a >= b && a >= c) {
-			return a;
-		} else if(b >= a && b >= c) {
-			return b;
-		} else {
-			return c;
-		}
-	}
-	
 	public static int match(char a, char b) {
 		if(a == b) {
 			return 1;
@@ -174,5 +125,6 @@ public class Main {
 			return -1;
 		}
 	}
+	
 
 }
--- BioInformatique/src/model/Matrice.java
+++ BioInformatique/src/model/Matrice.java
@@ -1,540 +1,91 @@
 package model;
 
-/** Définition de la classe Matrice (Matrix) dans lequel nous trouvons beaucoup
- * de méthodes, d'opérations sur les matrices
- * 
- * Remarque : 
- * Cette classe est seulement un outils dans le but d'effectuer des calculs sur
- * les graphes. Ceci n'est pas une classe complète pour la manipulation de  
- * matrice.
- * 
- * @author ASLAN Hikmet
- * @version 1.1
- */
+public class Matrice {
+	private int[][] m;
+	private int countLigne;
+	private int countColonne;
+	
+	public Matrice(String f1, String f2) {
+		countLigne = f1.length() + 1;
+		countColonne = f2.length() + 1;
+		m = new int[countLigne][countColonne];
+		calcul(f1, f2);
+	}
+	
+	public void calcul(String f1, String f2) {
+		for(int i = 1; i < countLigne; i++) {
+			for(int j = 1; j < countColonne; j++) {
+				m[i][j] = max(
+							m[i-1][j] - 2, // Faire une constante avec le score d'un GAP
+							m[i][j-1] - 2,
+							m[i-1][j-1] + match(f1.charAt(i-1), f2.charAt(j-1))
+						);
+			}
+		}
+	}
+	
+	public int max(int a, int b, int c) {
+		if(a >= b && a >= c) {
+			return a;
+		} else if(b >= a && b >= c) {
+			return b;
+		} else {
+			return c;
+		}
+	}
+	
+	public int match(char a, char b) {
+		if(a == b) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+	
+	public void display() {
+		for(int i = 0; i < countLigne; i++) {
+			for(int j = 0; j < countColonne; j++) {		
+				System.out.print(m[i][j]+"  ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public Position maxMatrice() {
+		// max Colonne
+		int maxColonne = 0;
+		int ligne = -1;
+		for(int i = 0; i < countLigne; i++) {	
+			int comp = m[i][countColonne - 1];
+			if(comp > maxColonne) {
+				maxColonne = comp;
+				ligne = i;
+			}
+		}
+		// max Ligne
+		int maxLigne = 0;
+		int colonne = -1;
+		for(int i = 0; i < countColonne; i++) {	
+			int comp = m[countLigne - 1][i];
+			if(comp > maxLigne) {
+				maxLigne = comp;
+				colonne = i;
+			}
+		}
 
-public class Matrice
-{
-   private long[][] coeff = null;
-   private int diametre = 0;
-   private int distance = 0;
-   
-   //----------------------------------------------//
-   //                       CONSTRUCTOR                            //
-   //----------------------------------------------//
-   /** Constructeur Matrice
-     * @param
-     * int i - ligne
-     * int j - colonne 
-     */
-   public Matrice(int i, int j)
-   {
-       this.setLength(i,j);
-   }
-   
-   public Matrice()
-   {
-       this(0,0);
-   }
-
-   public Matrice(long[][] mat)
-   {
-       this.coeff = mat;
-   }
-
-   //----------------------------------------------//
-   //                               SETTER                           //
-   //----------------------------------------------//    
-   // définit une matrice de type long[][]
-   public void setMatrice(long[][] mat)
-   {
-       this.coeff = mat;
-   }
-   
-   // définit une valeur �  la position i et j
-   // i - ligne
-   // j - col
-   public void setValue(int i, int j, long value)
-   {
-       this.coeff[i][j] = value;
-   }
-   
-   
-   // on définit la taille de la mtrice
-   public void setLength(int i, int j)
-   {
-       this.coeff = new long[i][j];
-   }
-   
-   
-   //----------------------------------------------//
-   //                               GETTER                           //
-   //----------------------------------------------//    
-   // retourne la matrice sous forme du type long[][]
-   public long[][] getMatrice()
-   {
-       return this.coeff;
-   }
-   
-   // retourne le nombre de ligne
-   public int getRows()
-   {
-       return this.coeff.length;
-   }
-   
-   // retourne le nombre de colonne
-   public int getColumns()
-   {
-       return this.coeff[0].length;
-   }
-   
-   // retourne la valeur �  la position i et j
-   public long getValue(int i, int j)
-   {
-       return this.coeff[i][j];
-   }
-   
-   // retourne le déterminant d'une matrice
-   public long getDeterminant()
-   {
-       Matrice a = null;
-       long value = 0;
-   
-       if (this.getRows() < 3 && this.getColumns() < 3)
-           return (this.getValue(0,0)*this.getValue(1,1) - this.getValue(1,0)*this.getValue(0,1));
-       
-       
-       for (int j=0; j<this.getColumns(); j++)
-       {
-               a = this.getNewMatrice(0,j);
-               value += (int)Math.pow(-1,j)*(this.getValue(0,j)*a.getDeterminant());
-       }
-       
-       return value;
-   }
-   
-   // retourne la matrice inverse de la matrice this
-   public Matrice getMatriceInverse()
-   {
-       Matrice a = new Matrice(this.getRows(), this.getColumns());
-       Matrice tmp = null;
-       long det = this.getDeterminant();
-       
-       for (int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-           {
-               tmp = this.getNewMatrice(i,j);
-               a.setValue(i,j,(int)Math.pow(-1,i+j)*(tmp.getDeterminant()/det));
-           }
-           
-       // on transpose la matrice les coeffcients seront positionné de façon incorrect
-       return a.getMatriceTranspose();
-   }
-   
-   
-   /* Retourne une nouvelle matrice mais en supprimant 
-    * la ligne row et la colonne columns
-    */
-   private Matrice getNewMatrice(int row, int columns)
-   {
-       Matrice a = new Matrice(this.getRows()-1, this.getColumns()-1);
-       int k = -1, m = 0;
-       
-       for (int i=0; i<this.getRows(); i++)
-       {
-           
-           k++;
-           
-           if (i == row) 
-           {    
-               k--;
-                continue;
-           }
-
-           m = -1;
-           
-           for (int j=0; j<this.getColumns(); j++)
-           {
-           
-               m++;
-                           
-               if (j==columns) 
-               {
-                   m--;
-                   continue;                    
-               }
-               
-               a.setValue(k,m,this.getValue(i,j));
-           }        
-
-       }
-       
-       return a;
-   }
-   
-   /**
-     * Retourne le nombre de combinaison a partir d'une matrice
-     * définit a partir d'un graphe.
-     * @param 
-     *     sA - sommet A
-     *    sB - sommet B
-     *    nb - Nombre de arrete (ou nombre de caractere du combinaison)
-     * @return
-     *     long - nombre de combinaison possible entre 2 sommets
-     */
-   public long getGrapheCombiCount(int sA, int sB, int nb)
-   {
-       if (sB > this.getRows() || sA > this.getColumns())
-           return -1;
-   
-       Matrice a = this.matricePow(nb);
-       
-       return a.getValue(sB-1,sA-1);
-   }
-   
-   
-   // retourne la matrice I en fonction de la mtrice this
-   public Matrice getMatriceIdentity()
-   {
-       Matrice a = new Matrice(this.getRows(),this.getColumns());
-       
-       for (int i=0; i<this.getRows(); i++)
-           a.setValue(i,i,1);
-       
-       return a;        
-   }
-   
-   // transpose la matrice 
-   public Matrice getMatriceTranspose()
-   {
-       Matrice a = new Matrice(this.getColumns(), this.getRows());
-       long tmp = 0;
-       
-       for (int i=0; i<a.getRows(); i++)
-           for (int j=0; j<a.getColumns(); j++)
-           {
-               tmp = this.getValue(j,i);
-               a.setValue(i,j,tmp);
-           }
-       
-       
-       return a;
-   }
-   
-   // retourne la valeur de la trice de la matrice
-   public long getTraceMatrice()
-   {
-       long value = 0;
-       
-       for (int i=0; i<this.getRows(); i++)
-           value += this.getValue(i,i);
-       
-       return value;
-   }
-   
-   
-   /** Retourne la distance (nombre d'arrete) entre 
-     * entre deux sommets sA et sB, tel que sA <= sB
-     * si sA > sB, méthode renvoi -1 pour erreur
-     */
-   public long getDistanceGraphe(int sA, int sB)
-   {
-       long value = 0;
-       
-       if (sA > sB) 
-           return -1;
-       
-       if (this.getValue(sB-1,sA-1) != 0)
-           return (this.getValue(sB-1,sA-1));
-       
-       for (int i=sA; i<sB; i++)
-           value += this.getValue(i-1, (i+1)-1);
-
-       return value;
-   }
-   
-   // GRAPHE
-   // retourne la matrice de distance 
-   public Matrice getMatriceDistanceGraphe()
-   {
-       Matrice a = new Matrice(this.getRows(),this.getColumns());
-       int n=1;
-       
-       for (int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-           {
-                if (a.getValue(i,j) == 0)
-                       a = this.matricePow(n++);
-
-           }
-
-       // n-1 correspond a la distance 
-       this.distance = n-1;
-   
-       return a;
-   }
-   
-   // GRAPHE
-   // retourne la distance de la matrice
-   public int getDistance()
-   {
-       this.getMatriceDistanceGraphe();
-       return this.distance;
-   }
-   
-   
-   // retourne la matrice compagnon en fonction de la matrice this
-   public Matrice getMatriceCompagnon()
-   {
-       Matrice a = new Matrice(this.getRows(),this.getColumns());
-       
-       for (int i=0; i<a.getRows()-1; i++)
-           a.setValue(i+1,i,1);
-       
-           a.setValue(this.getRows()-1,this.getColumns()-1,-1);                
-                   
-       return a;
-   }
-   
-   
-   // GRAPHE
-   // retourne la matrice diam
-   public Matrice getMatriceDiametre()
-   {
-       int n=1;
-       Matrice ai = this.sumMatrice(this.getMatriceIdentity());
-
-       for (int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-           {
-                if (ai.getValue(i,j) == 0)
-                       ai = this.matricePow(n++);
-
-           }
-           
-       this.diametre = n-1;
-       
-       return ai;
-   }
-   
-   // GRAPHE
-   // retourne la valeur du diametre de la matrice (ou graphe :))
-   public int getDiametre()
-   {
-       this.getMatriceDiametre();
-       return this.diametre;
-   }
-   
-   // GRAPHE
-   // retourne les deux sommets les plus éloignés
-   public int[] getSommetPLusDistant()
-   {
-       int[] sommets = new int[2];
-       Matrice m = this.matricePow(this.getDiametre()-1);
-       byte n = 0;
-       
-       for (int i=0; i<m.getRows(); i++)
-           for (int j=0; j<m.getColumns(); j++)
-               if (m.getValue(i,j) == 0)
-               {
-                   sommets[n++] = i+1;
-               }
-       
-       return sommets;
-   }
-   
-   //----------------------------------------------//
-   //                         OTHERS METHODS                   //
-   //----------------------------------------------//    
-   // multiplication
-   public Matrice multiply(final Matrice matrice)
-   {
-       Matrice a = new Matrice(this.getRows(), this.getColumns());
-       int k,i,j,m;
-       long value = 0;
-               
-       for (k=0; k<this.getColumns(); k++)
-       {
-                       
-           for (i=0; i<this.getRows(); i++)
-           {
-           
-               for (j=0; j<this.getColumns(); j++)
-                   value += this.getValue(i,j)*matrice.getValue(j,k);
-
-               a.setValue(i,k,value);
-               value = 0;
-           }
-       }
-       
-       return a;
-   }
-   
-   // addition
-   public Matrice sumMatrice(final Matrice matrice)
-   {
-       Matrice a = new Matrice(this.getRows(), this.getColumns());
-       
-       for (int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-               a.setValue(i,j,this.getValue(i,j)+matrice.getValue(i,j));
-           
-       return a;
-   }
-   
-   // puissance -> M^n
-   public Matrice matricePow(int n)
-   {
-       Matrice a = this;
-       Matrice b = a;
-       
-       for (int i=0; i<n-1; i++)
-           b = a.multiply(b);
-           
-       return b;
-   }
-   
-   // soustraction
-   public Matrice soustraction(final Matrice matrice)
-   {
-       Matrice a = new Matrice(this.getRows(), this.getColumns());
-       
-       for (int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-               a.setValue(i,j,this.getValue(i,j)-matrice.getValue(i,j));
-           
-       return a;    
-   }
-   
-   // multiplication d'une matrice par une constante k
-   public Matrice multiplyByK(long k)
-   {
-       Matrice a = this;
-       
-       for (int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-               a.setValue(i,j,this.getValue(i,j)*k);
-       
-       return a;
-   }
-   
-   
-   // division d'une matrice par une constante k
-   public Matrice divByK(long k)
-   {
-       Matrice a = this;
-       
-       for (int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-               a.setValue(i,j,this.getValue(i,j)/k);
-       
-       return a;
-   }
-   
-   // le fameux toString() :)
-   public String toString()
-   {
-       String out = "";
-   
-       for (int i=0; i<this.getRows(); i++)
-       {
-           for (int j=0; j<this.getColumns(); j++)
-               out +=this.coeff[i][j]+"\t ";
-               
-               out+="\n";
-       }
-               
-       return out;
-   }
-   
-   
-   // definit si deux matrices sont équivalentes
-   public boolean equals(Matrice matrice)
-   {
-       for(int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-               if (this.getValue(i,j) != matrice.getValue(i,j))
-                       return false;
-       
-       return true;
-   } 
-   
-   //----------------------------------------------//
-   //                          METHODS IS...                       //
-   //----------------------------------------------//    
-   // détermine si la matrice est symetrique
-   public boolean isSymetric()
-   {
-       if (this.getRows() == this.getColumns())
-           return false;
-           
-       for(int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-               if (this.getValue(i,j) != this.getValue(j,i))
-                       return false;
-                       
-       return true;                
-   }
-   
-   // détermine si la matrice est triangulaire
-   public boolean isTriangularMatrix()
-   {
-       for(int i=0; i<this.getRows(); i++)
-           for (int j=1; j<this.getColumns(); j++)
-               if (this.getValue(i,j) != 0)
-                       return false;        
-       
-       return true;
-   }
-   
-   // détermine si la matrice est une matrice unité
-   public boolean isUnitMatrix()
-   {        
-       return (this.equals(this.getMatriceIdentity()));
-   }
-   
-   // détermine si la matrice est inversible
-   public boolean isInversible()
-   {
-       return (this.getDeterminant() != 0);
-   }
-   
-   // determine si la mtrice contient au moins une valeur 0
-   public boolean isZero()
-   {
-       for(int i=0; i<this.getRows(); i++)
-           for (int j=0; j<this.getColumns(); j++)
-               if (this.getValue(i,j) == 0)
-                       return true;            
-                       
-       return false;
-   }
-   
-   //----------------------------------------------//
-   //                      LAUNCH PROGRAM                       //
-   //----------------------------------------------//
-   public static void main(String[] args)
-   {
-           // matrice d'adjacence d'un graphe
-           Matrice a = new Matrice();
-           a.setMatrice(new long[][] { {0,1,1,0,0,0,0,0,0,0,0}, // 1
-                                                {1,0,1,1,1,0,0,0,0,0,0}, // 2
-                                                {1,1,0,0,1,0,0,0,0,0,0}, // 3
-                                                {0,1,0,0,0,1,0,0,0,0,0}, // 4
-                                                {0,1,1,0,0,0,1,0,0,0,0}, // 5
-                                                {0,0,0,1,0,0,1,1,1,0,0}, // 6
-                                                {0,0,0,0,1,1,0,0,0,1,0}, // 7
-                                                {0,0,0,0,0,1,0,0,1,0,1}, // 8
-                                                {0,0,0,0,0,1,0,1,0,1,1}, // 9
-                                                {0,0,0,0,0,0,1,0,1,0,1}, // 10
-                                                {0,0,0,0,0,0,0,1,1,1,0}  // 11
-                                               });
-                                                               
-       //System.out.println("Matrice A : \n"+a);                
-       //System.out.println("Distance : \n"+a.matricePow(4));
-       
-       Matrice x = new Matrice(new long[][] { {0,1,0,0}, {1,0,0,0}, {0,0,1,1},{0,0,0,1}});
-       System.out.println("Det : \n" + x.getDeterminant());
-       System.out.println("Matrice inverse : \n" + x.getMatriceInverse());
-
-   }
+		return maxLigne > maxColonne ? new Position(countLigne - 1, colonne) : new Position(ligne, countColonne - 1);
+	}
+	
+	public int getCountLigne() {
+		return countLigne;
+	}
+	
+	public int getCountColonne() {
+		return countColonne;
+	}
+	
+	public int[][] getMatrice() {
+		return m;
+	}
+	
 }
