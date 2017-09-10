53dc8929c59d100070dd7baf53a0801bc173495a
Mikis Millemaci <Mikis Millemaci>
2017-09-10 23:47:34 +0200

    update 10/09/17

--- BioInformatique/src/main/Main.java
+++ BioInformatique/src/main/Main.java
@@ -6,7 +6,7 @@ class Main {
 
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
-		new Controller(args[0], args[1], args[2]);
+		new Controller(args[0], args[2], args[4]);
 		long finish = System.currentTimeMillis();
 		System.out.println("Methode executee en " + Long.toString(finish - start) + " millisecondes");
 	}
